0cb227ef479a4604092d97c5cfc4270e0a4e0741
Neil Garc√≠a Vargas <Neil@neils-macbook-pro.local>
2015-09-18 21:19:45 -0600

    Initial Commit

---
+++ WhittedRaytracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WhittedRaytracer
+//
+//  Created by Neil García Vargas on 9/18/15.
+//  Copyright © 2015 Neil García Vargas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
